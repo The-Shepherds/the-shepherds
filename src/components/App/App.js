1a6cd061c8eda5a901ad6b501b66a9322211d0d9
Stephen Burggraaf <steedsranch@gmail.com>
2026-05-09 09:57:50 -0700

    better backgrounds

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -12,9 +12,7 @@ import { FaXTwitter } from "react-icons/fa6";
 function App() {
   return (
     <main id="app">
-
       <header className="topNav">
-
         <a
           href="https://rumble.com"
           target="_blank"
@@ -63,33 +61,27 @@ function App() {
           <FaFacebookF className="navIcon" />
           <span>Facebook</span>
         </a>
-
       </header>
 
       <h1>The Shepherds</h1>
 
-      <section className="heroSection">
+      <img
+        src={shepherdLogo}
+        alt="The Shepherds Logo"
+        id="heroImage"
+        loading="lazy"
+      />
 
-        <img
-          src={shepherdLogo}
-          alt="The Shepherds Logo"
-          id="heroImage"
-          loading="lazy"
-        />
-
+      <section className="proverbsSection">
         <p className="quote">
-          “Iron sharpeneth iron; so a man sharpeneth the
-          countenance of his friend.”
+          “Iron sharpeneth iron; so a man sharpeneth the countenance of his
+          friend.”
         </p>
 
-        <p className="verse">
-          ~Proverbs 27:17
-        </p>
-
+        <p className="verse">~Proverbs 27:17</p>
       </section>
 
       <section className="codeSection">
-
         <h2>Code of Unlocking Human Potential</h2>
 
         <p className="mission">
@@ -97,58 +89,28 @@ function App() {
         </p>
 
         <ol className="duties">
-
-          <li>
-            We have a duty to humanity to survive.
-          </li>
-
-          <li>
-            We have a duty to prioritize humanity’s prosperity.
-          </li>
-
-          <li>
-            We have a duty to understand the entirety of humanity.
-          </li>
-
-          <li>
-            We have a duty to critically think and speak.
-          </li>
-
+          <li>We have a duty to humanity to survive.</li>
+          <li>We have a duty to prioritize humanity’s prosperity.</li>
+          <li>We have a duty to understand the entirety of humanity.</li>
+          <li>We have a duty to critically think and speak.</li>
           <li>
             We have a duty to never stop disciplined learning and teaching.
           </li>
-
           <li>
             We have a duty to take full responsibility for all our actions.
           </li>
-
           <li>
             We have a duty to understand reality as it is, not as we wish it.
           </li>
-
-          <li>
-            We have a duty to have no deception.
-          </li>
-
-          <li>
-            We have a duty to persuade and never manipulate.
-          </li>
-
-          <li>
-            We have a duty to discover threats and imposters.
-          </li>
-
-          <li>
-            We have a duty to persist in artistic expression.
-          </li>
-
+          <li>We have a duty to have no deception.</li>
+          <li>We have a duty to persuade and never manipulate.</li>
+          <li>We have a duty to discover threats and imposters.</li>
+          <li>We have a duty to persist in artistic expression.</li>
           <li>
             We have a duty to humanity to have moderation in the majority of
             things.
           </li>
-
         </ol>
-
       </section>
 
       <p className="quote gladiatorQuote">
@@ -163,7 +125,6 @@ function App() {
         <br />
         ~The Movie: Gladiator
       </p>
-
     </main>
   );
 }
